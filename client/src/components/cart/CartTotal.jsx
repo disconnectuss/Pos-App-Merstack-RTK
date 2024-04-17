c3ac23e7105769def653985f2fbe16dfbead92b9
disconnectus <fka201520@gmail.com>
2024-04-17 14:14:00 +0300

    orders completed

--- client/src/components/cart/CartTotal.jsx
+++ client/src/components/cart/CartTotal.jsx
@@ -1,11 +1,16 @@
-import { Button } from "antd";
+import { Button, message } from "antd";
 import {
   ClearOutlined,
   PlusCircleOutlined,
   MinusCircleOutlined,
 } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
-import { deleteFromCart } from "../../redux/slices/cartSlice";
+import {
+  deleteFromCart,
+  increment,
+  decrement,
+  reset,
+} from "../../redux/slices/cartSlice";
 
 const CartTotal = () => {
   const cart = useSelector((state) => state.cart);
@@ -15,43 +20,77 @@ const CartTotal = () => {
     <div className="cart h-full max-h-[calc(100vh_-_120px)] flex flex-col">
       {/* calculation can be flexible */}
       <h2 className="bg-blue-500 text-center py-4 text-white font-bold tracking-wide">
-        Orders
+        Basket
       </h2>
       <ul className="cart-items px-2 flex flex-col gap-y-3 pt-2 overflow-y-auto">
-        {cart.cartItem.map((item) => (
-          <li className="cart-item flex justify-between" key={item._id}>
-            <div className="flex items-center">
-              <img
-                className="w-16 h-16 object-cover cursor-pointer"
-                src={item.img}
-                alt=""
-                onClick={() => dispatch(deleteFromCart(item))}
-              />
-              <div className="flex flex-col ml-2">
-                <b>{item.title}</b>
-                <span>
-                  {" "}
-                  {item.price} € x {item.quantity}{" "}
-                </span>
-              </div>
-            </div>
-            <div className="flex items-center gap-x-1">
-              <Button
-                type="primary"
-                size="small"
-                className="w-full flex items-center justify-center !rounded-full !text-black"
-                icon={<PlusCircleOutlined />}
-              />
-              <span className="font-bold">{item.quantity}</span>
-              <Button
-                type="primary"
-                size="small"
-                className="w-full flex items-center justify-center !rounded-full !text-black"
-                icon={<MinusCircleOutlined />}
-              />
-            </div>
-          </li>
-        ))}
+        {cart.cartItem.length > 0
+          ? cart.cartItem.map((item) => (
+              <li className="cart-item flex justify-between" key={item._id}>
+                <div className="flex items-center">
+                  <img
+                    className="w-16 h-16 object-cover cursor-pointer"
+                    src={item.img}
+                    alt=""
+                    onClick={() => {
+                      dispatch(deleteFromCart(item));
+                      message.success("Successfully deleted!");
+                    }}
+                  />
+                  <div className="flex flex-col ml-2">
+                    <b>{item.title}</b>
+                    <span>
+                      {" "}
+                      {item.price} € x {item.quantity}{" "}
+                    </span>
+                  </div>
+                </div>
+                <div className="flex items-center gap-x-1">
+                  <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full !text-black"
+                    icon={
+                      <PlusCircleOutlined
+                        onClick={() => {
+                          dispatch(increment(item));
+                          message.success("Successfully added!");
+                        }}
+                      />
+                    }
+                  />
+                  <span className="font-bold">{item.quantity}</span>
+                  <Button
+                    type="primary"
+                    size="small"
+                    className="w-full flex items-center justify-center !rounded-full !text-black"
+                    icon={
+                      item.quantity === 1 ? (
+                        <MinusCircleOutlined
+                          onClick={() => {
+                            if (
+                              window.confirm(
+                                "Are you sure you want to delete this item?"
+                              )
+                            ) {
+                              dispatch(decrement(item));
+                              message.success("Successfully deleted!");
+                            }
+                          }}
+                        />
+                      ) : (
+                        <MinusCircleOutlined
+                          onClick={() => {
+                            dispatch(decrement(item));
+                            message.success("Successfully deleted");
+                          }}
+                        />
+                      )
+                    }
+                  />
+                </div>
+              </li>
+            ))
+          : "No order yet!"}
       </ul>
       <div className="cart-totals mt-auto">
         <div className="border-t border-b">
@@ -71,15 +110,23 @@ const CartTotal = () => {
           <div className="border-b mt-4">
             <div className="flex justify-between p-2 border-t">
               <b>Cart Total</b>
-              <span>{cart.total + (cart.total * cart.tax) / 100 >0
-              ? (cart.total + (cart.total * cart.tax )/ 100).toFixed(2)
-            : 0} €</span>
+              <span>
+                {cart.total + (cart.total * cart.tax) / 100 > 0
+                  ? (cart.total + (cart.total * cart.tax) / 100).toFixed(2)
+                  : 0}{" "}
+                €
+              </span>
             </div>
           </div>
         </div>
       </div>
       <div className="py-4 px-2 ">
-        <Button type="primary" size="large" className="addToOrder w-full">
+        <Button
+          type="primary"
+          size="large"
+          className="addToOrder w-full"
+          disabled={cart.cartItem.length === 0}
+        >
           Add to Order
         </Button>
         <Button
@@ -88,6 +135,13 @@ const CartTotal = () => {
           className="w-full mt-2 flex items-center justify-center"
           icon={<ClearOutlined />}
           danger
+          disabled={cart.cartItem.length === 0}
+          onClick={() => {
+            if (window.confirm("Are you sure?")) {
+              dispatch(reset());
+              message.success("Successfully deleted");
+            }
+          }}
         >
           Delete
         </Button>
--- client/src/redux/slices/cartSlice.js
+++ client/src/redux/slices/cartSlice.js
@@ -1,9 +1,9 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  cartItem: [], 
+  cartItem: [],
   total: 0,
-  price:0,
+  price: 0,
   tax: 5,
 };
 
@@ -13,30 +13,54 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state = initialState, action) => {
       const findCartItem = state.cartItem.find(
-        (item)=> item._id === action.payload._id
-      )
-      if (findCartItem){
+        (item) => item._id === action.payload._id
+      );
+      if (findCartItem) {
         findCartItem.quantity = findCartItem.quantity + 1;
       } else {
         state.cartItem.push(action.payload);
       }
-      state.total += action.payload.price
+      state.total += action.payload.price;
     },
     deleteFromCart: (state = initialState, action) => {
       state.cartItem = state.cartItem.filter(
-        (item)=> item._id !== action.payload._id
-      )
+        (item) => item._id !== action.payload._id
+      );
       /* const findCartItemIndex = state.cartItem.findIndex(
         (item)=> item._id === action.payload._id
       )
       if (findCartItemIndex !== -1) {
         state.cartItem.splice(findCartItemIndex, 1);
       } */
-      state.total -= action.payload.price * action.payload.quantity
+      state.total -= action.payload.price * action.payload.quantity;
     },
-  },
+    increment: (state = initialState, action) => {
+      const findItem = state.cartItem.find(
+        (item) => item._id === action.payload._id
+      );
+      findItem.quantity += 1;
+      state.total += findItem.price;
+    },
+    decrement: (state = initialState, action) => {
+      const findItem = state.cartItem.find(
+        (item) => item._id === action.payload._id
+      );
+      findItem.quantity -= 1;
+      if (findItem.quantity === 0) {
+        state.cartItem = state.cartItem.filter(
+          (item) => item._id !== action.payload._id
+        );
+      }
+      state.total -= findItem.price;
+    },
+    reset: (state = initialState) => {
+      state.cartItem = [];
+      state.total = 0;
+    },
+  }  
 });
 
-export const { addToCart, deleteFromCart } = cartSlice.actions;
+export const { addToCart, deleteFromCart, increment, decrement, reset } =
+  cartSlice.actions;
 
 export default cartSlice.reducer;
